34d9ecdd590eb047c86bf5ccf50cf65837eb069b
Willian <willsbit@gmail.com>
2022-07-01 20:06:34 -0300

    [FEATURE] Develop minimal page to present the catdex

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result};
+use actix_files::{Files, NamedFile};
+use handlebars::Handlebars;
+use serde_json::json;
+
+
+async fn index(hb: web::Data<Handlebars<'_>>) -> HttpResponse {
+    let data = json!({
+        "project_name": "Catdex",
+        "cats": [
+            {
+                "name": "British short hair",
+                "image_path": "/static/image/british-short-hair.jpg"
+            },
+            {
+                "name": "Persian",
+                "image_path": "/static/image/persian.jpg"
+            },
+            {
+                "name": "Ragdoll",
+                "image_path": "/static/image/ragdoll.jpg"
+            },
+        ]
+    });
+
+    let body = hb.render("index", &data).unwrap();
+    HttpResponse::Ok().body(body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_templates_directory(".html", "./static/")
+        .unwrap();
+
+    let handlebars_ref = web::Data::new(handlebars);
+
+    println!("Listening on port 8080");
+    HttpServer::new(move || {
+        App::new()
+            .app_data(handlebars_ref.clone())
+            .service(
+                Files::new("/static", "static")
+                    .show_files_listing(), // handy for debugging, but should be turned off in production
+            )
+            .route("/", web::get().to(index))
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
