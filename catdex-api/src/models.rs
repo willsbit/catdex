7c4e74651931b39178dd097b586b31873170dbd2
Willian <willsbit@gmail.com>
2022-07-07 17:51:10 -0300

    [INTERNAL] Setup database schema and ORM model

---
+++ catdex-api/src/models.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+use super::schema::cats;
+
+#[derive(Queryable, Serialize)]
+pub struct Cat {
+    pub id: i32,
+    pub name: String,
+    pub image_path: String
+}
+
+#[derive(Insertable, Serialize, Deserialize)]
+#[table_name = "cats"]
+pub struct NewCat {
+    // id is SERIAL type, database will handle it
+    pub name: String,
+    pub image_path: String
+}
