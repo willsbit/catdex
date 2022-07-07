53b3c56773d63f0c41b74c0f583731269312a322
Willian <willsbit@gmail.com>
2022-07-07 15:40:59 -0300

    [INTERNAL] Adjust up/down migration scripts

--- migrations/2022-07-04-001706_create_cats/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
-CREATE TABLE cats (
-                      id SERIAL PRIMARY KEY,
-                      name VARCHAR NOT NULL,
-                      image_path VARCHAR NOT NULL
-)
