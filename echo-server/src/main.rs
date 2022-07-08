9e526d249d31bec3a7fb9bcc87aef88445bcd882
Willian <willsbit@gmail.com>
2022-07-08 16:39:07 -0300

    [FEATURE] Send notifications periodically

--- echo-server/src/main.rs
+++ echo-server/src/main.rs
@@ -1,11 +1,23 @@
-use ws::*;
+use ws::{Handler, Sender, WebSocket};
+use std::{thread, time};
+
+struct Server {
+    out: Sender
+}
+
+impl Handler for Server {}
 
 fn main() {
-    ws::listen("127.0.0.1:8080", |out| {
-        move |msg| {
-            println!("Received message: {}", msg);
-            out.send(msg)
-        }
-    })
-    .unwrap()
+    let server = WebSocket::new(|out| Server { out }).unwrap();
+
+    let broadcaster = server.broadcaster();
+
+    let periodic = thread::spawn(move || loop {
+        broadcaster.send("Meow!").unwrap();
+        thread::sleep(time::Duration::from_secs(1));
+    });
+
+    server.listen("127.0.0.1:8080").unwrap();
+
+    periodic.join().unwrap()
 }
