34828f9304b4cd45f0a462521a0b1039b097046f
Willian <willsbit@gmail.com>
2022-07-08 14:38:38 -0300

    [FEATURE] Input validation and a bit of refactoring

--- catdex-api/src/main.rs
+++ catdex-api/src/main.rs
@@ -3,10 +3,12 @@ extern crate diesel;
 
 use actix_files::Files;
 use actix_web::{http, web, App, Error, HttpServer, Responder, HttpResponse, web::{Data}};
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 use std::env;
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
+use validator::Validate;
+use validator_derive::Validate;
 
 mod models;
 mod schema;
@@ -15,6 +17,37 @@ use self::schema::cats::dsl::*;
 type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 
+/// Struct to hold the cat id's.
+#[derive(Deserialize, Validate)]
+struct CatEndpointPath {
+    #[validate(range(min = 1, max = 150))]
+    id: i32,
+}
+
+/// API endpoint to show details from a specific cat
+async fn cat_endpoint(
+    pool: web::Data<DbPool>,
+    cat_id: web::Path<CatEndpointPath>
+) -> Result<HttpResponse, Error> {
+
+    cat_id
+        .validate()
+        .map_err(|_| HttpResponse::BadRequest().finish()).unwrap();
+
+
+    let connection = pool.get()
+        .expect("Can't get DB connection from pool");
+
+    let cat_data = web::block(move || {
+        cats.filter(id.eq(cat_id.id)).first::<Cat>(&connection)
+    })
+    .await
+    .map_err(|_| HttpResponse::InternalServerError().finish()).unwrap().unwrap();
+
+    Ok(HttpResponse::Ok().json(cat_data))
+}
+
+/// Endpoint to get 100 cats from the database
 async fn cats_endpoint(
         pool: web::Data<DbPool>,
     ) -> Result<HttpResponse, Error> {
@@ -28,7 +61,7 @@ async fn cats_endpoint(
         return Ok(HttpResponse::Ok().json(cats_data));
     }
 
-
+/// Sets up a connecion pool with the Postgres instance
 fn setup_database() -> DbPool {
     let database_url = env::var("DATABASE_URL")
             .expect("DATABASE_URL must be set");
@@ -40,6 +73,15 @@ fn setup_database() -> DbPool {
             .expect("Failed to create DB connection pool.")
 }
 
+/// Convenience function to avoid repeated code
+fn api_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/api")
+        .route("/cats", web::get().to(cats_endpoint))
+        .route("/cat/{id}", web::get().to(cat_endpoint))
+    );
+}
+
     #[actix_web::main]
     async fn main() -> std::io::Result<()> {
         
@@ -49,12 +91,7 @@ fn setup_database() -> DbPool {
         HttpServer::new(move || {
             App::new()
                 .app_data(Data::new(pool.clone()))
-                .service(
-                    web::scope("/api").route(
-                        "/cats",
-                        web::get().to(cats_endpoint),
-                    ),
-                )
+                .configure(api_config)
                 .service(
                     Files::new("/", "static").show_files_listing(),
                 )
@@ -70,14 +107,12 @@ fn setup_database() -> DbPool {
         use super::*;
         use actix_web::{test, App};
         use actix_rt::*;
-        
+
         #[actix_rt::test]
         async fn test_cats_endpoint_get() {
             let pool = setup_database();
             let mut app = test::init_service(
-                App::new().app_data(Data::new(pool.clone()))
-                    .route("/api/cats", web::get().to(cats_endpoint)
-                )
+                App::new().app_data(Data::new(pool.clone())).configure(api_config)
             )
             .await;
 
