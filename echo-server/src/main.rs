57f6961be0d7ee2122f67801686c3e4283dbccca
Willian <willsbit@gmail.com>
2022-07-08 16:33:50 -0300

    [FEATURE] Implement a minimal websocket echo server

---
+++ echo-server/src/main.rs
@@ -0,0 +1,11 @@
+use ws::*;
+
+fn main() {
+    ws::listen("127.0.0.1:8080", |out| {
+        move |msg| {
+            println!("Received message: {}", msg);
+            out.send(msg)
+        }
+    })
+    .unwrap()
+}
